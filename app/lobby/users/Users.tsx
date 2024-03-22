a36defaea0d121f3e8d725bcb761d05b14e9603a
Sawwy <kimmo.j.taskinen@gmail.com>
2024-03-22 15:38:38 +0200

    showing users online / idle on user list

--- app/lobby/users/Users.tsx
+++ app/lobby/users/Users.tsx
@@ -1,5 +1,70 @@
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
+type UserType = {
+  isOnline: boolean;
+  lastSeen: Date;
+  joined: Date;
+  username: string;
+  avatar: string;
+  description: string;
+};
+
+interface UserResponseType extends Omit<UserType, "lastSeen" | "joined"> {
+  lastSeen: string;
+  joined: string;
+}
+
+const deserializeUsersResponse = (data: UserResponseType[]) => {
+  const deserialized = data.map((user) => {
+    return {
+      ...user,
+      lastSeen: new Date(user.lastSeen),
+      joined: new Date(user.joined),
+    };
+  });
+
+  return deserialized;
+};
+
 export const Users = () => {
-  return <div className={styles.users}>Users</div>;
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("/api/v1/users");
+      if (response.ok) {
+        const data: UserResponseType[] = await response.json();
+        setUsers(deserializeUsersResponse(data));
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const idleTreshold = new Date();
+  idleTreshold.setMinutes(-15);
+
+  return (
+    <div className={styles.users}>
+      {users.map((user) => {
+        return (
+          <div className={styles.user} key={user.username}>
+            <div className={styles.avatar}>
+              <img alt={`avatar of user ${user.username}`} src={user.avatar} />
+              {user.isOnline && (
+                <div className={styles.status}>
+                  <div className={styles.online} />
+                  {user.lastSeen > idleTreshold && (
+                    <div className={styles.idle} />
+                  )}
+                </div>
+              )}
+            </div>
+            <span>{user.username}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -6,4 +6,41 @@ export const handlers = [
     // return HttpResponse.json({ message: "Unauthorized" }, { status: 401 });
     return HttpResponse.json({ roomToken: "asdf-1234" });
   }),
+  http.get("/api/v1/users", async () => {
+    await delay("real");
+
+    const date1 = new Date();
+    date1.setMinutes(-20);
+    const date2 = new Date();
+    date2.setMinutes(-5);
+    const date3 = new Date();
+    date3.setHours(-7);
+
+    return HttpResponse.json([
+      {
+        isOnline: true,
+        lastSeen: date1.toJSON(),
+        joined: date1.toJSON(),
+        username: "qwerty",
+        avatar: "https://ui-avatars.com/api/?name=Qwer&size=32",
+        description: "",
+      },
+      {
+        isOnline: true,
+        lastSeen: date2.toJSON(),
+        joined: date2.toJSON(),
+        username: "blarblar",
+        avatar: "https://ui-avatars.com/api/?name=Blar&size=32",
+        description: "",
+      },
+      {
+        isOnline: false,
+        lastSeen: date3.toJSON(),
+        joined: date3.toJSON(),
+        username: "dendi",
+        avatar: "https://ui-avatars.com/api/?name=Dendi&size=32",
+        description: "",
+      },
+    ]);
+  }),
 ];

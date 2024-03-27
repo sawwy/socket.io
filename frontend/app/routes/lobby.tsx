870bf577be808f995f500c0e760fce6fb67daf50
Sawwy <kimmo.j.taskinen@gmail.com>
2024-03-27 11:47:39 +0000

    [workaround]: Navigate to joinroom on page refresh

--- frontend/app/routes/lobby.tsx
+++ frontend/app/routes/lobby.tsx
@@ -1,5 +1,16 @@
+import { useNavigate } from "@remix-run/react";
+import { useContext, useEffect } from "react";
 import { Lobby } from "~/Lobby/Lobby";
+import SocketContext from "~/contexts/Socket/Context";
 
 export default function LobbyRoute() {
+  const { username } = useContext(SocketContext).SocketState;
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log("username", username);
+    if (!username) {
+      navigate("/joinroom");
+    }
+  }, [username]);
   return <Lobby />;
 }
